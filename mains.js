a64ef1e97e7b9c42490b2aa1cb442f0ecaf01a6a
trongbao66 <trongbao662005@gmail.com>
2023-08-22 20:48:24 +0700

    message

---
+++ mains.js
@@ -0,0 +1,22 @@
+// bai 1 
+// var a = 10;
+// var b = 15;
+// var c = 25;
+// var d = ( a + b + c)/3
+// var 
+// console.log (d)
+
+// // bai 2 
+// var a = 1 
+// var b = '2'
+// var c = ' tran van bao trong'
+// console.log(a + b) // ép a về string xong nối chuỗi 
+// console.log(b - a) // ép b về number xong rồi thực hiện phép toán
+// console.log(a + c) // giống a + b 
+
+// toán tử so sánh 
+// var a = 30 
+// var b = 20
+// var c = 'đáp án đúng'
+// var d = 'đáp án sai'
+// if ( a > b ) console.log (d)
